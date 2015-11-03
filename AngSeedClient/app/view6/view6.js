c3bb081013a4d082cd7d0a9c8f5c11508eb23e7d
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-11-03 22:05:03 +0100

    added start controller code for create new user form

--- AngSeedClient/app/view6/view6.js
+++ AngSeedClient/app/view6/view6.js
@@ -2,14 +2,14 @@
 
 angular.module('myApp.view6', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view6', {
-    templateUrl: 'view6/view6.html',
-    controller: 'View6Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view6', {
+                    templateUrl: 'view6/view6.html',
+                    controller: 'View6Ctrl'
+                });
+            }])
 
-//.controller('View6Ctrl', function($http,$scope) {
+        .controller('View6Ctrl', function ($http, $scope) {
 //  $http.get('api/demoadmin')
 //            .success(function (data, status, headers, config) {
 //              $scope.data = data;
@@ -17,5 +17,13 @@ angular.module('myApp.view6', ['ngRoute'])
 //            .error(function (data, status, headers, config) {
 //              
 //             });
-// 
-//});
+//
+            $scope.addUser = function () {
+                var data = JSON.stringify($scope.user);
+
+                $http.post("api/demouser", data).success(function (data, status) {
+                    $scope.hello = data;
+                })
+            }
+
+        });
