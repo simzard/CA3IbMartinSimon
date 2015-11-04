a389733a23d46c27b366aaad3cf7e3196d86bc41
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-11-04 12:59:54 +0100

    added ui for company TODO make local REST api

--- AngSeedClient/app/view3/view3.js
+++ AngSeedClient/app/view3/view3.js
@@ -2,14 +2,15 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view3', {
+                    templateUrl: 'view3/view3.html',
+                    controller: 'View3Ctrl'
+                });
+            }])
 
-//.controller('View3Ctrl', function($http,$scope) {
+        .controller('View3Ctrl', function ($http, $scope) {
+
 //  $http.get('api/demoadmin')
 //            .success(function (data, status, headers, config) {
 //              $scope.data = data;
@@ -17,5 +18,45 @@ angular.module('myApp.view3', ['ngRoute'])
 //            .error(function (data, status, headers, config) {
 //              
 //             });
-// 
-//});
+
+            $scope.countries = [{
+                    value: 'dk',
+                    label: 'dk'
+                }, {
+                    value: 'nor',
+                    label: 'nor'
+                }];
+            
+            $scope.options = [{
+                    value: 'vat',
+                    label: 'vat'
+                }, {
+                    value: 'name',
+                    label: 'name'
+                }, {
+                    value: 'produ',
+                    label: 'produ'
+                }, {
+                    value: 'phone',
+                    label: 'phone'
+                }];
+
+
+
+            $scope.find = function () {
+                document.domain = "localhost";
+                
+                $scope.optionsString = "?";
+                $scope.optionsString += $scope.optionList.label + "=" + $scope.search;
+                $scope.optionsString += "&country=" + $scope.countryList.label;
+                
+                $http.get('http://cvrapi.dk/api' + $scope.optionsString)
+                        .success(function (data, status, headers, config) {
+                            $scope.data = data;
+                        })
+                        .error(function (data, status, headers, config) {
+
+                        });
+            }
+
+        });
