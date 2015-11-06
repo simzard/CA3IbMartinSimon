61f1d0cbedc786e0425a922764eeab317769746e
Simon Steinaa <thatguy@simonsteinaa.dk>
2015-11-06 15:22:49 +0100

    PART 2 UI now updated

--- AngSeedClient/app/view3/view3.js
+++ AngSeedClient/app/view3/view3.js
@@ -3,11 +3,15 @@
 angular.module('myApp.view3', ['ngRoute'])
 
         .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view3', {
+                $routeProvider
+                .when('/view3', {
                     templateUrl: 'view3/view3.html',
                     controller: 'View3Ctrl'
-                });
+                })
+                
             }])
+
+        
 
         .controller('View3Ctrl', function ($http, $scope) {
 
